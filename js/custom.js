41ae616b7dad57787c28e49e840e315aeed29779
vnipi20 <60774199+vnipi20@users.noreply.github.com>
2020-08-05 10:56:37 +0300

    Подключил попап

--- js/custom.js
+++ js/custom.js
@@ -1,21 +1,13 @@
 /*--popup--*/
-let popup = document.getElementById('mypopup');
-let popupToggle = document.getElementById('myBtn');
-let popupClose = document.querySelector('.close');
-
-popupToggle.onclick = function () {
-    popup.style.display = 'block';
-};
-
-popupClose.onclick = function () {
-    popup.style.display = 'none';
-};
-
-window.onclick =function (e) {
-    if (e.target == popup) {
-        popup.style.display = 'none';
-    }
-};
+$(document).ready(function(){
+    PopUpHide();
+});
+function PopUpShow(){
+    $("#popup1").show();
+}
+function PopUpHide(){
+    $("#popup1").hide();
+}
 /*--popup--*/
 
 
