b2ae6b77286af5fb0036f48552165a0eae58f8ef
vnipi20 <60774199+vnipi20@users.noreply.github.com>
2020-08-13 13:36:54 +0300

    Перенес кнопку вверх

--- js/custom.js
+++ js/custom.js
@@ -1,45 +1,55 @@
 /*--popup--*/
-$(document).ready(function(){
+$(document).ready(function () {
     PopUpHide();
 });
-function PopUpShow(){
+
+function PopUpShow() {
     $("#popup1").show();
 }
-function PopUpHide(){
+
+function PopUpHide() {
     $("#popup1").hide();
 }
-/*--popup--*/
 
+/*--добавление данные светильников с файла json при нажатии на тип светильника--*/
 var btn1 = document.getElementById('btn1');
 var btn2 = document.getElementById('btn2');
 var btn3 = document.getElementById('btn3');
 var btn4 = document.getElementById('btn4');
 var btn5 = document.getElementById('btn5');
 var btn6 = document.getElementById('btn6');
+/*--добавление ККС с файла json при нажатии на наименование светильника--*/
+var lt001 = document.getElementById('lt001');
 
-btn1.addEventListener('click', function () {
+lt001.addEventListener('click', function () {
 
-    $(document).ready(function () {
-        var getSomeDate = function () {
-            var date;
 
-            $.ajax({
-                url: 'PRS_R_ECO_LED.json',
-                dataType: 'json',
-                success: function (resp) {
-                    date = resp;
-                    updatePost(date)
-                }
-            });
-            return date;
-        };
+     });
 
-        getSomeDate();
+    /*--добавление данные светильников с файла json при нажатии на тип светильника--*/
+    btn1.addEventListener('click', function () {
 
-        function updatePost(date) {
-            var posts = date.map((el, item) => {
+        $(document).ready(function () {
+            var getSomeDate = function () {
+                var date;
 
-                return (`
+                $.ajax({
+                    url: 'PRS_R_ECO_LED.json',
+                    dataType: 'json',
+                    success: function (resp) {
+                        date = resp;
+                        updatePost(date)
+                    }
+                });
+                return date;
+            };
+
+            getSomeDate();
+
+            function updatePost(date) {
+                var posts = date.map((el, item) => {
+
+                    return (`
                      <div class="item_wrap-name">
                             <h1 class="item_name">${el.PRS_R_ECO_LED[0].mainTypeLighting}</h1>
                             <h1 class="item_name"> ${el.PRS_R_ECO_LED[0].mainNameLighting}</h1>
@@ -51,37 +61,37 @@ btn1.addEventListener('click', function () {
                             <h1 class="item_name"> ${el.PRS_R_ECO_LED[2].PRS_R_ECO_LED_595[0].lightFlow}</h1>
                         </div>
                          `)
-            });
+                });
 
-            $('#item-wrapper').append(posts);
-        }
+                $('#item-wrapper').append(posts);
+            }
 
-    })
-});
+        })
+    });
 
-btn2.addEventListener('click', function () {
+    btn2.addEventListener('click', function () {
 
-    $(document).ready(function () {
-        var getSomeDate = function () {
-            var date;
+        $(document).ready(function () {
+            var getSomeDate = function () {
+                var date;
 
-            $.ajax({
-                url: 'PRS_R_ECO_LED.json',
-                dataType: 'json',
-                success: function (resp) {
-                    date = resp;
-                    updatePost(date)
-                }
-            });
-            return date;
-        };
+                $.ajax({
+                    url: 'PRS_R_ECO_LED.json',
+                    dataType: 'json',
+                    success: function (resp) {
+                        date = resp;
+                        updatePost(date)
+                    }
+                });
+                return date;
+            };
 
-        getSomeDate();
+            getSomeDate();
 
-        function updatePost(date) {
-            var posts = date.map((el, item) => {
+            function updatePost(date) {
+                var posts = date.map((el, item) => {
 
-                return (`
+                    return (`
                      <div class="item_wrap-name">
                             <h1 class="item_name">${el.PRS_R_ECO_LED[0].mainTypeLighting}</h1>
                             <h1 class="item_name"> ${el.PRS_R_ECO_LED[0].mainNameLighting}</h1>
@@ -93,37 +103,37 @@ btn2.addEventListener('click', function () {
                             <h1 class="item_name"> ${el.PRS_R_ECO_LED[2].PRS_R_ECO_LED_595_A[0].lightFlow}</h1>
                         </div>
                          `)
-            });
+                });
 
-            $('#item-wrapper').append(posts);
-        }
+                $('#item-wrapper').append(posts);
+            }
 
-    })
-});
+        })
+    });
 
-btn3.addEventListener('click', function () {
+    btn3.addEventListener('click', function () {
 
-    $(document).ready(function () {
-        var getSomeDate = function () {
-            var date;
+        $(document).ready(function () {
+            var getSomeDate = function () {
+                var date;
 
-            $.ajax({
-                url: 'PRS_R_ECO_LED.json',
-                dataType: 'json',
-                success: function (resp) {
-                    date = resp;
-                    updatePost(date)
-                }
-            });
-            return date;
-        };
+                $.ajax({
+                    url: 'PRS_R_ECO_LED.json',
+                    dataType: 'json',
+                    success: function (resp) {
+                        date = resp;
+                        updatePost(date)
+                    }
+                });
+                return date;
+            };
 
-        getSomeDate();
+            getSomeDate();
 
-        function updatePost(date) {
-            var posts = date.map((el, item) => {
+            function updatePost(date) {
+                var posts = date.map((el, item) => {
 
-                return (`
+                    return (`
                      <div class="item_wrap-name">
                             <h1 class="item_name">${el.PRS_R_ECO_LED[0].mainTypeLighting}</h1>
                             <h1 class="item_name"> ${el.PRS_R_ECO_LED[0].mainNameLighting}</h1>
@@ -135,37 +145,37 @@ btn3.addEventListener('click', function () {
                             <h1 class="item_name"> ${el.PRS_R_ECO_LED[2].PRS_R_ECO_LED_595_HFD_4000K[0].lightFlow}</h1>
                         </div>
                          `)
-            });
+                });
 
-            $('#item-wrapper').append(posts);
-        }
+                $('#item-wrapper').append(posts);
+            }
 
-    })
-});
+        })
+    });
 
-btn4.addEventListener('click', function () {
+    btn4.addEventListener('click', function () {
 
-    $(document).ready(function () {
-        var getSomeDate = function () {
-            var date;
+        $(document).ready(function () {
+            var getSomeDate = function () {
+                var date;
 
-            $.ajax({
-                url: 'PRS_R_ECO_LED.json',
-                dataType: 'json',
-                success: function (resp) {
-                    date = resp;
-                    updatePost(date)
-                }
-            });
-            return date;
-        };
+                $.ajax({
+                    url: 'PRS_R_ECO_LED.json',
+                    dataType: 'json',
+                    success: function (resp) {
+                        date = resp;
+                        updatePost(date)
+                    }
+                });
+                return date;
+            };
 
-        getSomeDate();
+            getSomeDate();
 
-        function updatePost(date) {
-            var posts = date.map((el, item) => {
+            function updatePost(date) {
+                var posts = date.map((el, item) => {
 
-                return (`
+                    return (`
                      <div class="item_wrap-name">
                             <h1 class="item_name">${el.PRS_R_ECO_LED[0].mainTypeLighting}</h1>
                             <h1 class="item_name"> ${el.PRS_R_ECO_LED[0].mainNameLighting}</h1>
@@ -177,36 +187,36 @@ btn4.addEventListener('click', function () {
                             <h1 class="item_name"> ${el.PRS_R_ECO_LED[2].PRS_R_ECO_LED_595_HFD_EM_4000K[0].lightFlow}</h1>
                         </div>
                          `)
-            });
+                });
 
-            $('#item-wrapper').append(posts);
-        }
+                $('#item-wrapper').append(posts);
+            }
 
-    })
-});
-btn5.addEventListener('click', function () {
+        })
+    });
+    btn5.addEventListener('click', function () {
 
-    $(document).ready(function () {
-        var getSomeDate = function () {
-            var date;
+        $(document).ready(function () {
+            var getSomeDate = function () {
+                var date;
 
-            $.ajax({
-                url: 'AOT.OPL_ECO_LED.json',
-                dataType: 'json',
-                success: function (resp) {
-                    date = resp;
-                    updatePost(date)
-                }
-            });
-                        return date;
-        };
+                $.ajax({
+                    url: 'AOT.OPL_ECO_LED.json',
+                    dataType: 'json',
+                    success: function (resp) {
+                        date = resp;
+                        updatePost(date)
+                    }
+                });
+                return date;
+            };
 
-        getSomeDate();
+            getSomeDate();
 
-        function updatePost(date) {
-            var posts = date.map((el, item) => {
+            function updatePost(date) {
+                var posts = date.map((el, item) => {
 
-                return (`
+                    return (`
                      <div class="item_wrap-name">
                             <h1 class="item_name">${el.AOT_OPL_ECO_LED[0].mainTypeLighting}</h1>
                             <h1 class="item_name"> ${el.AOT_OPL_ECO_LED[0].mainNameLighting}</h1>
@@ -218,36 +228,36 @@ btn5.addEventListener('click', function () {
                             <h1 class="item_name"> ${el.AOT_OPL_ECO_LED[2].AOT_OPL_ECO_LED_600_EM_4000K[0].lightFlow}</h1>
                         </div>
                          `)
-            });
+                });
 
-            $('#item-wrapper').append(posts);
-        }
+                $('#item-wrapper').append(posts);
+            }
 
-    })
-});
-btn6.addEventListener('click', function () {
+        })
+    });
+    btn6.addEventListener('click', function () {
 
-    $(document).ready(function () {
-        var getSomeDate = function () {
-            var date;
+        $(document).ready(function () {
+            var getSomeDate = function () {
+                var date;
 
-            $.ajax({
-                url: 'AOT.OPL_ECO_LED.json',
-                dataType: 'json',
-                success: function (resp) {
-                    date = resp;
-                    updatePost(date)
-                }
-            });
-            return date;
-        };
+                $.ajax({
+                    url: 'AOT.OPL_ECO_LED.json',
+                    dataType: 'json',
+                    success: function (resp) {
+                        date = resp;
+                        updatePost(date)
+                    }
+                });
+                return date;
+            };
 
-        getSomeDate();
+            getSomeDate();
 
-        function updatePost(date) {
-            var posts = date.map((el, item) => {
+            function updatePost(date) {
+                var posts = date.map((el, item) => {
 
-                return (`
+                    return (`
                      <div class="item_wrap-name">
                             <h1 class="item_name">${el.AOT_OPL_ECO_LED[0].mainTypeLighting}</h1>
                             <h1 class="item_name"> ${el.AOT_OPL_ECO_LED[0].mainNameLighting}</h1>
@@ -259,10 +269,12 @@ btn6.addEventListener('click', function () {
                             <h1 class="item_name"> ${el.AOT_OPL_ECO_LED[2].AOT_OPL_ECO_LED_600_EM_4000K[0].lightFlow}</h1>
                         </div>
                          `)
-            });
+                });
 
-            $('#item-wrapper').append(posts);
-        }
+                $('#item-wrapper').append(posts);
+            }
 
-    })
-});
+        })
+    });
+
+
